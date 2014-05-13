7de3f6270be89888cda098a5811c6c4ff5456bd4
Jason Hall <imjasonh@gmail.com>
2014-05-13 11:10:05 -0400

    Rename getter, add godoc

--- secrets.go
+++ secrets.go
@@ -14,9 +14,9 @@
 package secrets
 
 import (
-        "appengine"
-        "appengine/datastore"
-        "appengine/memcache"
+	"appengine"
+	"appengine/datastore"
+	"appengine/memcache"
 )
 
 // Entity name used to store secrets in the datastore.
@@ -27,42 +27,46 @@ var MemcacheKeyPrefix = ""
 
 var local = map[string]string{}
 
-func Secret(c appengine.Context, key string) string {
-        if v, ok := local[key]; ok {
-                // Found secret in instance memory, return it.
-                return v
-        }
+// Get returns the secret value associated with the key.
+//
+// If the value cannot be found in any of local instance memory, memcache, or
+// the datastore, an empty string will be returned.
+func Get(c appengine.Context, key string) string {
+	if v, ok := local[key]; ok {
+		// Found secret in instance memory, return it.
+		return v
+	}
 
-        i, err := memcache.Get(c, key)
-        if err == nil {
-                // Found secret in memcache, return it.
-                return string(i.Value)
-        }
+	i, err := memcache.Get(c, key)
+	if err == nil {
+		// Found secret in memcache, return it.
+		return string(i.Value)
+	}
 
-        if err != nil && err != memcache.ErrCacheMiss {
-                c.Errorf("error getting %q from memcache: %v", key, err)
-        }
+	if err != nil && err != memcache.ErrCacheMiss {
+		c.Errorf("error getting %q from memcache: %v", key, err)
+	}
 
-        // Get secret from datastore if missing from memcache.
-        k := datastore.NewKey(c, EntityName, key, 0, nil)
-        var e struct {
-                Value string `datastore:"-"`
-        }
-        if err := datastore.Get(c, k, &e); err != nil {
-                c.Errorf("error getting %q from datastore: %v", key, err)
-                return ""
-        }
+	// Get secret from datastore if missing from memcache.
+	k := datastore.NewKey(c, EntityName, key, 0, nil)
+	var e struct {
+		Value string `datastore:"-"`
+	}
+	if err := datastore.Get(c, k, &e); err != nil {
+		c.Errorf("error getting %q from datastore: %v", key, err)
+		return ""
+	}
 
-        // Store secret in instance memory for next time.
-        local[key] = e.Value
+	// Store secret in instance memory for next time.
+	local[key] = e.Value
 
-        // Store secret in memcache for next time.
-        if err := memcache.Set(c, &memcache.Item{
-                Key:   MemcacheKeyPrefix + key,
-                Value: []byte(e.Value),
-        }); err != nil {
-                c.Errorf("error setting %q in memcache: %v", key, err)
-        }
+	// Store secret in memcache for next time.
+	if err := memcache.Set(c, &memcache.Item{
+		Key:   MemcacheKeyPrefix + key,
+		Value: []byte(e.Value),
+	}); err != nil {
+		c.Errorf("error setting %q in memcache: %v", key, err)
+	}
 
-        return e.Value
+	return e.Value
 }
