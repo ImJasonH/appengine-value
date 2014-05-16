e11bda8d6025cb4f5d07de4b905835a6d8bcc912
Jason Hall <imjasonh@gmail.com>
2014-05-16 16:34:35 -0400

    add Set method, which cannot update already set values, and must be run by an admin

--- value.go
+++ value.go
@@ -3,9 +3,9 @@
 // Package value provides a utility method to retrieve string values in an
 // App Engine app, so they can be stored in the datastore instead of in source.
 //
-// Values should be set in the datastore using the App Engine Admin UI, and will
-// be cached in memcache and local instance memory for quick lookup. Values
-// should not be changed once they are set.
+// Values can be inserted (but not updated) using Set, or by using the App Engine
+// Admin UI. Once retrieved, values will be cached in memcache and local instance
+// memory for quick lookup. Values cannot be updated, except via the Admin UI.
 //
 // Intended use cases are OAuth client secrets or API keys, for instance, which
 // are used across many requests are should be quick to look up, but shouldn't
@@ -16,9 +16,12 @@
 package value
 
 import (
+	"errors"
+
 	"appengine"
 	"appengine/datastore"
 	"appengine/memcache"
+	"appengine/user"
 )
 
 // Entity name used to store values in the datastore.
@@ -28,6 +31,10 @@ var EntityName = "Values"
 var MemcacheKeyPrefix = ""
 
 var local = map[string]string{}
+
+type e struct {
+	Value string `datastore:",noindex"`
+}
 
 // Get returns the value associated with the key.
 //
@@ -51,9 +58,7 @@ func Get(c appengine.Context, key string) string {
 
 	// Get value from datastore if missing from memcache.
 	k := datastore.NewKey(c, EntityName, key, 0, nil)
-	var e struct {
-		Value string `datastore:",noindex"`
-	}
+	var e e
 	if err := datastore.Get(c, k, &e); err != nil {
 		c.Errorf("error getting %q from datastore: %v", key, err)
 		return ""
@@ -71,4 +76,32 @@ func Get(c appengine.Context, key string) string {
 	}
 
 	return e.Value
+}
+
+func Set(c appengine.Context, key string, val string) error {
+	if !user.IsAdmin(c) {
+		return errors.New("only admin users can set values")
+	}
+
+	if _, ok := local[key]; ok {
+		return errors.New("key found in local instance")
+	}
+	if _, err := memcache.Get(c, key); err != memcache.ErrCacheMiss {
+		return errors.New("key found in memcache")
+	}
+	return datastore.RunInTransaction(c, func(tc appengine.Context) error {
+		// Fail if the value is already stored.
+		k := datastore.NewKey(c, EntityName, key, 0, nil)
+		if err := datastore.Get(tc, k, nil); err == nil {
+			return errors.New("key found in datastore")
+		} else if err == datastore.ErrNoSuchEntity {
+			// Okay, good. Keep going.
+		} else if err != nil {
+			return err
+		}
+
+		// Put the value in the datastore.
+		_, err := datastore.Put(tc, k, e{Value: key})
+		return err
+	})
 }
